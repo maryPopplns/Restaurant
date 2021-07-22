891b62deb23b10114a20f4bfd5abe9c19ee6a146
Spencer Knight <spencer.knightusn@gmail.com>
2021-07-21 17:33:31 -0700

    removed element borders changed text font

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -11,9 +11,14 @@ const NAV = () => {
   const LIST_ITEMS = ["home", "menu", "catering"];
   for (let i = 0; i < LIST_ITEMS.length; i++) {
     const LI = document.createElement("li");
+    const BUTTON = document.createElement("button");
     LI.setAttribute("id", LIST_ITEMS[i]);
     LI.setAttribute("class", "nav_menu");
+    BUTTON.setAttribute("id", `${LIST_ITEMS[i]}_button`);
+    BUTTON.setAttribute("class", "nav_button");
+    BUTTON.innerText = LIST_ITEMS[i];
     UL.append(LI);
+    LI.append(BUTTON);
   }
 };
 
